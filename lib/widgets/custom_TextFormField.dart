8fea6a1895aad3024a77afbfcb4bd98812528f7f
Mfas1998 <mfas7381@gmail.com>
2023-04-14 23:45:05 +0300

    customized  height of  TextFormField

--- lib/widgets/custom_TextFormField.dart
+++ lib/widgets/custom_TextFormField.dart
@@ -1,33 +1,35 @@
 import 'package:e_commerce/core/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 
-Container custom_TextFormField({required String label,String? hintText}) => Container(
-    padding: const EdgeInsets.all(12),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          label,
-          style: const TextStyle(
-              color: AppColors.textColor,
-              fontSize: 18,
-              fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        TextFormField(
-          decoration: InputDecoration(
-            hintText: hintText,
-            enabledBorder: OutlineInputBorder(
-              borderSide: const BorderSide(color: AppColors.grayColor),
-              borderRadius: BorderRadius.circular(10),
+Container custom_TextFormField({required String label, String? hintText}) =>
+    Container(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              label,
+              style: const TextStyle(
+                  color: AppColors.textColor,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
             ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: const BorderSide(color: AppColors.primaryColor),
-              borderRadius: BorderRadius.circular(10),
+            const SizedBox(
+              height: 10,
             ),
-          ),
-        )
-      ],
-    ));
+            TextFormField(
+              decoration: InputDecoration(
+                isDense: true,
+                hintText: hintText,
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: AppColors.grayColor),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: AppColors.primaryColor),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            )
+          ],
+        ));
