f8eb271a58650c6a538c53c78d64dfa09573df1c
Hdjeheud <abwalzalramsy240@gmail.com>
2023-04-19 01:26:42 +0300

    upload my pages

---
+++ lib/auth.dart
@@ -0,0 +1,29 @@
+import 'package:e_commerce/register.dart';
+import 'package:e_commerce/view/home_screen.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class Auth extends StatefulWidget {
+  const Auth({super.key});
+
+  @override
+  State<Auth> createState() => _AuthState();
+}
+
+class _AuthState extends State<Auth> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        //body: StreamBuilder<User?>(
+        // stream: FirebaseAuth.instance.authStateChanges(),
+        // builder: ((context, snapshot) {
+        //  if (snapshot.hasData) {
+        //  return HomeScreen();
+        // } else {
+        // return Register();
+        // }
+        // }),
+        // ),
+        );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,33 @@ import 'package:e_commerce/view/home_screen.dart';
 import 'package:e_commerce/view/notification%20_Feed.dart';
 import 'package:e_commerce/view/notification.dart';
 import 'package:e_commerce/view/notification_Activity.dart';
+//import 'package:e_commerce/view/register.dart';
 import 'package:e_commerce/view/search.dart';
+
 // import 'package:e_commerce/view/delete_address_confirmation.dart';
 
 import 'package:flutter/material.dart';
 
 import 'app.dart';
+import 'login.dart';
+
+import 'register.dart';
+import 'splash_screen.dart';
 import 'view/search_result_three.dart';
 
 void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Splash_Screen(),
+    );
+  }
+}
